2328f88d77651be9b6dddf0ca4ef5a622c60bccb
Gabriel Veit <gabriel.veit99@gmail.com>
2021-03-19 11:59:37 -0400

    added some changes, tried to fix the dom error

--- src/index.js
+++ src/index.js
@@ -6,41 +6,45 @@ import App from './App';
 
 ReactDOM.render(
 
-  window.addEventListener('mousedown', function(e) {
-    document.body.classList.add('mouse-navigation');
-    document.body.classList.remove('kbd-navigation');
-  }),
+  render() {
+    return (
+      <App/>,
+      document.getElementById('root'),
   
-  window.addEventListener('keydown', function(e) {
-    if (e.keyCode === 9) {
-      document.body.classList.add('kbd-navigation');
-      document.body.classList.remove('mouse-navigation');
-    }
-  }),
+      window.addEventListener('mousedown', function(e) {
+        document.body.classList.add('mouse-navigation');
+        document.body.classList.remove('kbd-navigation');
+      }),
   
-  window.addEventListener('click', function(e) {
-    if (e.target.tagName === 'A' && e.target.getAttribute('href') === '#') {
-      e.preventDefault();
-    }
-  }),
-  /*
-  window.onerror = function(message, source, line, col, error) {
-    var text = error ? error.stack || error : message + ' (at ' + source + ':' + line + ':' + col + ')';
-    errors.textContent += text + '\n';
-    errors.style.display = '';
-  },
+      window.addEventListener('keydown', function(e) {
+        if (e.keyCode === 9) {
+          document.body.classList.add('kbd-navigation');
+          document.body.classList.remove('mouse-navigation');
+        }
+      }),
   
-  console.error = (function(old) {
-    return function error() {
-      errors.textContent += Array.prototype.slice.call(arguments).join(' ') + '\n';
-      errors.style.display = '';
-      old.apply(this, arguments);
-    }
-  })(console.error)
-*/
-
-
-
+      window.addEventListener('click', function(e) {
+        if (e.target.tagName === 'A' && e.target.getAttribute('href') === '#') {
+          e.preventDefault();
+        }
+      }),
+      /*
+      window.onerror = function(message, source, line, col, error) {
+        var text = error ? error.stack || error : message + ' (at ' + source + ':' + line + ':' + col + ')';
+        errors.textContent += text + '\n';
+        errors.style.display = '';
+      },
+  
+      console.error = (function(old) {
+        return function error() {
+          errors.textContent += Array.prototype.slice.call(arguments).join(' ') + '\n';
+          errors.style.display = '';
+          old.apply(this, arguments);
+        }
+      })(console.error)
+      */
+    );
+  }
 );
 
 
